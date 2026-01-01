783963c370b64dc0ead256c390709dbb85958c95
M0hamedAshraf19 <MohamedAshrafBedir@proton.me>
2026-01-01 11:39:24 +0200

    Update Json2Html.py

--- Json2Html.py
+++ Json2Html.py
@@ -18,9 +18,8 @@ else:
 	Entries=[]
 	for i in Tables:Entries.append(E1%(i,i))
 	with open("index.html","w") as f:f.write(P1%("".join(Entries).rstrip()))
-	print("Index updated")
+	print("Main page updated")
 
 	print("Finished")
-
 
 shutil.rmtree("__pycache__")
