79d672745120362018a2b79d53d402eea60e46e3
M0hamedAshraf19 <MohamedAshrafBedir@proton.me>
2026-01-01 11:34:48 +0200

    Add files via upload

---
+++ Json2Html.py
@@ -0,0 +1,25 @@
+import os,json,shutil
+from Vars import *
+Tables=[]
+for i in os.listdir():
+	if i.endswith(".json"):Tables.append(i[:-5])
+
+if Tables == []:
+	print("No data available")
+else:
+	print("Creating pages")
+	for i in Tables:
+		Entries=[]
+		with open("%s.json"%(i)) as f:d=json.load(f)
+		for n,l in zip(list(d.keys()), list(d.values())):Entries.append(E0%(n,l))
+		with open("%s.html"%(i),"w") as f:f.write(P0%(i,i,"".join(Entries).rstrip()))
+		print(i+".html created")
+
+	Entries=[]
+	for i in Tables:Entries.append(E1%(i,i))
+	with open("Index.html","w") as f:f.write(P1%("".join(Entries).rstrip()))
+	print("Index updated")
+
+	print("Finished")
+
+shutil.rmtree("__pycache__")
---
+++ Vars.py
@@ -0,0 +1,53 @@
+E0="""<tr>
+<td align="center">%s</td>
+<td align="center"><a href="%s">link</a></td>
+</tr>
+"""
+
+P0="""<html>
+<head>
+<title>%s</title>
+</head>
+<style>
+table, th, td {
+border:1px solid black;
+}
+</style>
+<body style="background-color:rgb(0, 195, 255)">
+<h1 align="center">%s</h1>
+<table align="center">
+<tr>
+<th>Name</th>
+<th>Website</th>
+</tr>
+%s
+</table>
+<p align="center">
+<a href="Index.html">Back to main page</a>
+</p>
+</body>
+</html>"""
+
+E1="""<font size="4">
+<a href="%s.html">%s</a>
+</font><br><br>"""
+
+P1="""<html>
+<head>
+<title>Welcome</title>
+</head>
+<style>
+</style>
+<body style="background-color:rgb(0, 195, 255)">
+<h1 align="center">Welcome</h1>
+<p align="center">
+<a href="ReadMe.txt">Introduction</a><br><br>
+</p>
+<h2 align="center">Index</h2>
+<p>
+<center>
+%s
+</center>
+</p>
+</body>
+</html>"""
