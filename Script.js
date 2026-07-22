416e753f7f0ce78929e383489f83cf85c293f57a
M0hamedAshraf19 <MohamedAshrafBedir@proton.me>
2026-07-22 09:14:16 +0300

    Edits

--- Script.js
+++ Script.js
@@ -1,7 +1,24 @@
 function openPopUp(id) {
-    document.getElementById(id).style.display='block'
+    document.getElementById(id).style.display='block';
 }
 
 function closePopUp(id) {
-    document.getElementById(id).style.display='none'
+    document.getElementById(id).style.display='none';
 }
+
+function switchItem(button) {
+    const item = button.parentElement
+    console.log(button.innerText, item);
+    const link = item.querySelector('a');
+    if (button.innerText == '-') {
+        button.innerText = '+'
+        button.style.top = '-5px'
+        link.style.display = 'none';
+    }
+    else {
+        button.innerText = '-'
+        button.style.top = '-15px'
+        link.style.display = '';
+    }
+}
+
